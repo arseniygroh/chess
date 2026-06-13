a8083a409b09513ad36191a390613f20861ef36b
Zaiats Andrii <andriy.s.zayats@gmail.com>
2026-06-14 00:22:55 +0300

    feat(model): make game models serializable for networking

--- src/main/java/chess/model/Position.java
+++ src/main/java/chess/model/Position.java
@@ -1,6 +1,8 @@
 package chess.model;
 
-public record Position(int row, int col) {
+import java.io.Serializable;
+
+public record Position(int row, int col) implements Serializable {
     public boolean isValid() {
         return row >= 0 && row < 8 && col >= 0 && col < 8;
     }
