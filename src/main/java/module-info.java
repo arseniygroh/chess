9e0fedf28454697b0c2b1d30092c58becf4ced0e
Zaiats Andrii <andriy.s.zayats@gmail.com>
2026-06-14 00:25:55 +0300

    build: configure maven and module-info for networking

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,11 @@ module chess {
 
     exports chess;
     exports chess.ui;
+    exports chess.network.protocol;
+    exports chess.network.client;
+    exports chess.server;
+
+    opens chess.model to java.base;
+    opens chess.model.pieces to java.base;
+    opens chess.network.protocol to java.base;
 }
