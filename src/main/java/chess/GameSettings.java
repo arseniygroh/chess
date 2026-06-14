34b4f2ceae36d11f79b6594a9512eb1a05c14033
Zaiats Andrii <andriy.s.zayats@gmail.com>
2026-06-14 18:38:18 +0300

    config: update server address to Railway proxy

--- src/main/java/chess/GameSettings.java
+++ src/main/java/chess/GameSettings.java
@@ -5,13 +5,12 @@ import chess.model.PlayerColor;
 
 public class GameSettings {
 
-    public static BotDifficulty botDifficulty =
-            BotDifficulty.MEDIUM;
+    public static BotDifficulty botDifficulty = BotDifficulty.MEDIUM;
     public static boolean isBotGame = true;
     public static boolean isNetworkGame = false;
     public static boolean isHost = false;
-    public static String serverAddress = "localhost";
-    public static int port = 12345;
+    public static String serverAddress = "acela.proxy.rlwy.net";
+    public static int port = 42877;
     public static PlayerColor playerColor = PlayerColor.WHITE;
     public static chess.network.protocol.UserProfile currentUser = null;
 }
